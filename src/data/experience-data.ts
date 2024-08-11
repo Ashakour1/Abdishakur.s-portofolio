12211ae622ab5f904c48f6075b27317ff3510bb9
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-12 02:00:55 +0300

    Add experience data for companies

---
+++ src/data/experience-data.ts
@@ -0,0 +1,28 @@
+import { type Experience } from "../types/experiences-type";
+
+export const ExperienceData: Experience[] = [
+  {
+    position: "Fullstack Developer",
+    name: "Matso Organization",
+    logo: "/logo-matso.jpg",
+    time: "Fep 2022 - July 2024",
+  },
+  {
+    position: "Founder & Developer",
+    name: "somAtlasApi",
+    logo: "/logo-somatlas.jpg",
+    time: "Dec 2023 - Present",
+  },
+  {
+    position: "Software Developer",
+    name: "Relia Trusty",
+    logo: "/logo-relia-trusty.jpg",
+    time: "Fep 2024 - Present",
+  },
+  {
+    position: "Chief Dev Officer",
+    name: "Jamhuriya Un Student - Jutsa",
+    logo: "/logo-jutsa.jpg",
+    time: "March 2024 - Present",
+  },
+];
