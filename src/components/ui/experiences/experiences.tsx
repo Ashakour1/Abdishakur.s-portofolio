23f4336d10cf72fa4886637dfbc59bce3ed8b664
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-18 20:37:39 +0300

    Add Experiences component for displaying experience data

---
+++ src/components/ui/experiences/experiences.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Experience from "./experience";
+import { ExperienceData } from "../../../data/experience-data";
+import { MdBusinessCenter } from "react-icons/md";
+
+const Experiences = () => {
+  return (
+    <section>
+      <div className="flex items-center text-center py-3 gap-1 text-gray-600">
+        <MdBusinessCenter className="text-xl" />
+        <h1 className="font-bold text-sm">Experience</h1>
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {ExperienceData.map((item, index) => (
+          <Experience key={index} experienceItem={item} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Experiences;
