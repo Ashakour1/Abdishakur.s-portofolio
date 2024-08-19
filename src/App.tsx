0fd1a8974a2fd1ad7912a4af6e7d3a17cd422d51
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-19 13:46:11 +0300

    Refactor component imports for improved code organization and code readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/ui/Header";
+import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/home-page";
+import HomePage from "./pages/HomeScreen";
 function App() {
   return (
     <>
