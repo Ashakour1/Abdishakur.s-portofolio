074d3625cdd0c4035ec393e8df6245bf7e1968d4
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-15 02:28:07 +0300

    Add sleep button to Header component and improve navigation styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { BiEnvelope, BiSearch } from "react-icons/bi";
 import { GoPerson } from "react-icons/go";
 import { Link } from "react-router-dom";
+import { GiNightSleep } from "react-icons/gi";
 
 const Header = () => {
   return (
     <header>
-      <div className="rounded-md flex justify-between items-center px-5 max-w-[1020px] mx-auto h-16 mt-2">
+      <div className="rounded-md flex  justify-between items-center px-5 max-w-[1020px] mx-auto h-16 mt-2">
         <div className="logo">
           <Link to="/">
             <img
@@ -15,15 +16,15 @@ const Header = () => {
             />
           </Link>
         </div>
-        <nav>
-          <ul className="flex space-x-4">
-            <div className="border  flex items-center text-center gap-1 px-2 py-1 rounded-md  hover:bg-gray-300 hover:border-none">
+        <nav className="border-[1px] border--gray-400 md:px-6 py-2  rounded-full">
+          <ul className="flex md:space-x-4 space-x-2">
+            <div className="  flex items-center text-center gap-1 px-2 py-1 rounded-md  hover:bg-gray-300 hover:-none">
               <GoPerson className="text-black text-xs" />
               <Link to="/" className=" text-black text-xs font-semibold">
                 About
               </Link>
             </div>
-            <div className="border  flex items-center text-center gap-1 px-2 py-1 rounded-md  hover:bg-gray-300 hover:border-none ">
+            <div className="  flex items-center text-center gap-1 px-2 py-1 rounded-md  hover:bg-gray-300 hover:-none ">
               <BiSearch className="text-black text-xs" />
               <Link
                 to="/projects"
@@ -32,7 +33,7 @@ const Header = () => {
                 Projects
               </Link>
             </div>
-            <div className="border flex items-center text-center gap-1 px-2 py-1 rounded-md hover:bg-gray-300 hover:border-none">
+            <div className=" flex items-center text-center gap-1 px-2 py-1 rounded-md hover:bg-gray-300 hover:border-none">
               <BiEnvelope className="text-black text-xs" />
               <Link to="/contact" className=" text-black text-xs font-semibold">
                 Contact
@@ -40,6 +41,11 @@ const Header = () => {
             </div>
           </ul>
         </nav>
+        <div className="flex items-center space-x-4">
+          <button className="border text-black px-3 py-3 rounded-full">
+            <GiNightSleep className="text-xl" />
+          </button>
+        </div>
       </div>
     </header>
   );
