95594d7cf53da7c80a9bd1d5c31757bda144c53d
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-15 02:04:16 +0300

    Replace project icon and update layout to dynamically render project items from ProjectsData

--- src/components/projects/ProjectsSection.tsx
+++ src/components/projects/ProjectsSection.tsx
@@ -1,15 +1,18 @@
-import { VscGithubProject } from "react-icons/vsc";
+import { AiFillProject } from "react-icons/ai";
 import Project from "./Project";
+import { ProjectsData } from "../../data/projects-data";
 
 const Projects = () => {
   return (
     <div>
-      <div className="flex items-center text-center py-3 gap-1 text-gray-600">
-        <VscGithubProject className="text-xl" />
+      <div className="flex items-center text-center py-5 gap-1 text-gray-600">
+        <AiFillProject className="text-xl" />
         <h1 className="font-bold text-sm">Projects</h1>
       </div>
-      <div className="gap-6">
-        <Project />
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap- gap-4">
+        {ProjectsData.map((project, index) => (
+          <Project key={index} projectItem={project} />
+        ))}
       </div>
     </div>
   );
