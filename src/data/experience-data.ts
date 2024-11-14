395484dee6301ac54b64acd480410147135dce92
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-15 02:03:37 +0300

    Add detailed descriptions and images to ExperienceData entries for enhanced clarity

--- src/data/experience-data.ts
+++ src/data/experience-data.ts
@@ -6,23 +6,40 @@ export const ExperienceData: Experience[] = [
     name: "Matso Organization",
     logo: "/logo-matso.jpg",
     time: "Fep 2022 - July 2024",
+    description:
+      "As a full-stack developer at Matso Organization, you'll design and develop high-quality software applications, collaborate with a skilled team, and stay updated with the latest industry trends. Your role involves writing efficient code, debugging issues, and delivering innovative solutions to maintain Matso Organization's leadership in the tech industry.",
+    technologies: ["React", "Node", "MongoDB", "Express"],
+    images: [],
   },
   {
     position: "Founder & Developer",
     name: "SomAtlasAPI",
     logo: "/logo-somatlas.jpg",
     time: "Dec 2023 - Present",
+    description:
+      "As the founder and developer of SomAtlasAPI, I developed a RESTful API to provide data on Somalia's regions, districts, and sub-districts. I designed the API to be user-friendly, secure, and scalable, ensuring that users can access accurate information with ease.",
+    technologies: ["React", "Node", "MongoDB", "Express"],
+    images: ["/landing3.png", "/docs.png"],
   },
   {
     position: "Software Developer",
     name: "Relia Trusty",
     logo: "/logo-relia-trusty.jpg",
     time: "Fep 2024 - July 2024",
+    description:
+      "As a software developer at Relia Trusty, you'll design and develop high-quality software applications, collaborate with a skilled team, and stay updated with the latest industry trends. Your role involves writing efficient code, debugging issues, and delivering innovative solutions to maintain Relia Trusty's leadership in the tech industry.",
+    technologies: ["System Analysis and Design"],
+    images: ["/landing5.png", "/uniso.png"],
   },
   {
     position: "Chief Dev Officer",
     name: "Jamhuriya Un Student - Jutsa",
     logo: "/logo-jutsa.jpg",
     time: "March 2024 - Present",
+    description:
+      "As the CDO of the Jamhuriya University Technology Students Association, I spearheaded digital advancements by developing a system to manage student data and events, creating the itDay event website to enhance participation, and implementing a secure voting system for efficient elections.",
+    technologies: ["React", "Node", "MongoDB", "Express"],
+    images: ["/client.png", "/about-client.png"],
   },
+  
 ];
