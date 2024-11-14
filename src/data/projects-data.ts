53c528083dd99533bc152079fe7b5fb457e44b33
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-15 02:03:29 +0300

    Add initial project data to ProjectsData for showcasing portfolio

---
+++ src/data/projects-data.ts
@@ -0,0 +1,40 @@
+import { type Project } from "../types/projects-type";
+
+export const ProjectsData: Project[] = [
+  {
+    name: "Aslos",
+    description:
+      "Matso Organization is a non-profit organization that aims to help the less privileged in the society.",
+    logo: "/logo-matso.jpg",
+    technologies: ["React", "Node", "MongoDB", "Express"],
+    github: "https://spotlight.tailwindui.com/projects",
+    demo: "https://spotlight.tailwindui.com/projects",
+  },
+  {
+    name: "SomAtlasAPI",
+    description:
+      "I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of",
+    logo: "/logo-somatlas.jpg",
+    technologies: ["React", "Node", "MongoDB", "Express"],
+    github: "https://spotlight.tailwindui.com/projects",
+    demo: "https://spotlight.tailwindui.com/projects",
+  },
+  {
+    name: "Relia Trusty",
+    description:
+      "I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of",
+    logo: "/logo-relia-trusty.jpg",
+    technologies: ["System Analysis and Design"],
+    github: "https://spotlight.tailwindui.com/projects",
+    demo: "https://spotlight.tailwindui.com/projects",
+  },
+  {
+    name: "Jamhuriya Un Student - Jutsa",
+    description:
+      "I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of",
+    logo: "/logo-jutsa.jpg",
+    technologies: ["React", "Node", "MongoDB", "Express"],
+    github: "https://spotlight.tailwindui.com/projects",
+    demo: "https://spotlight.tailwindui.com/projects",
+  },
+];
