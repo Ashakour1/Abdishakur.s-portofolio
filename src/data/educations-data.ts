ecfef97616e0dfedd8d0d33f7d6f279dad94b28b
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-15 14:09:16 +0300

    Add Educations data to showcase educational background

---
+++ src/data/educations-data.ts
@@ -0,0 +1,44 @@
+import { type Education } from "../types/educations-type";
+
+export const EducationsData: Education[] = [
+  {
+    degree: "Primary & Secondary",
+    institution: "Al imra primary and secondary",
+    date: "2012-2024",
+    description:
+      "I completed my education at Al-Imra Primary and Secondary School, where I built a strong academic foundation and developed essential skills that prepared me for future challenges",
+    location: "Mogadisho-Somalia",
+  },
+  {
+    degree: "Bachelor Of Computer Applications,",
+    institution: "Jamhuuriya University of Science and Technology",
+    date: "2021 - present",
+    description:
+      "I am currently in my seventh semester at Jamhuriya University of Science and Technology, pursuing a degree in Computer Science. I have a strong foundation in programming and software development and am a member of the IT and Computer Science department committee.",
+    location: "Mogadisho-Somalia",
+  },
+  {
+    degree: "CS50's Introduction to Computer Science",
+    institution: "HadvardX CS50x",
+    date: "2024-present",
+    description:
+      "Currently studying CS50's Introduction to Computer Science, a comprehensive online course that provides a strong foundation in programming, algorithms, and software development principles through hands-on projects and problem-solving.",
+    location: "Remote",
+  },
+  {
+    degree: "Fullstack Developer",
+    institution: "Dalbile Youth Initiative",
+    date: "12/2022 – 02/2023",
+    description:
+      "I graduated from Dalbile Youth and am currently honing my skills as a full-stack developer. I have a solid foundation in HTML, CSS, JavaScript, PHP, and MySQL, which I am actively expanding.",
+    location: "Mogadishu, Somalia",
+  },
+  {
+    degree: "User Experience (Ux)",
+    institution: "Coursera",
+    date: "09/2024 – present",
+    description:
+      "I am currently studying User Experience (UX) on Coursera, gaining practical skills in designing user-friendly digital products. The course covers user-centered design principles, interaction design, and usability testing. I’m working on creating wireframes, developing prototypes, and conducting user research to design intuitive and engaging experiences.",
+    location: "Remote",
+  },
+];
