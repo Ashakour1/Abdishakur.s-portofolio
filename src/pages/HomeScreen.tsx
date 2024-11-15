0d08c1e04612ebe6a6cd7fd37674e1675669639a
Ashakuur1 <engshakrayare114@gmail.com>
2024-11-15 14:09:25 +0300

    Update HomeScreen layout to include Educations component alongside Experiences

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -2,12 +2,16 @@ import Footer from "../components/Footer";
 import Experiences from "../components/experiences/experiences";
 import HeroSection from "../components/HeroSection";
 import Projects from "../components/projects/ProjectsSection";
+import Educations from "../components/educations/educations";
 
 const HomePage = () => {
   return (
     <div className="max-w-[1020px] mx-auto px-5">
       <HeroSection />
-      <Experiences />
+      <div className="flex flex-col gap-4 md:flex-row md:gap-4">
+        <Experiences />
+        <Educations />
+      </div>
       <Projects />
       <Footer />
     </div>
