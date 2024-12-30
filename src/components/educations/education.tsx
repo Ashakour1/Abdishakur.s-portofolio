52f21cdec0ae637455d5718ede778bc543e9c8fc
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-30 23:16:43 +0300

    Refactor Education component to use smaller text sizes for improved readability

--- src/components/educations/education.tsx
+++ src/components/educations/education.tsx
@@ -11,16 +11,18 @@ const Education = ({ educationItem }: EducationProps) => {
       <div className="flex flex-col">
         <h3 className="text-base font-bold">{educationItem.institution}</h3>
         <div className="flex items-center gap-2 mt-2 text-muted-foreground">
-          <span className="flex text-sm text-gray-600 items-center gap-1">
+          <span className="flex text-xs text-gray-600 items-center gap-1">
             <IoCalendarOutline className="w-4 h-4" />
             {educationItem.date}
           </span>
-          <span className="flex items-center text-sm text-gray-600 gap-1">
+          <span className="flex items-center text-xs text-gray-600 gap-1">
             <IoLocationOutline className="w-4 h-4" />
             {educationItem.location}
           </span>
         </div>
-        <p className="mt-2 text-sm text-gray-500">{educationItem.description}</p>
+        <p className="mt-2 text-xs text-gray-500">
+          {educationItem.description}
+        </p>
       </div>
     </section>
   );
