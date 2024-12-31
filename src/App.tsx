923ffdc062be20ffd20dd6719b089992964b6189
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-31 16:53:48 +0300

    Add Footer component and Toaster for improved user notifications

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import HomePage from "./pages/HomeScreen";
 import NotFound from "./components/NotFound";
 import ContactScreen from "./pages/ContactScreen";
 import ProjectsScreen from "./pages/ProjectsScreen";
+import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <>
+    <Toaster/>
       <Router>
         <Header />
         <Routes>
@@ -15,6 +18,7 @@ function App() {
           <Route path="/projects" element={<ProjectsScreen />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
