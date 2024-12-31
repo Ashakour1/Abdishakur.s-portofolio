a86855ad4102ad00f1a0ff9807f0cd70d11b4790
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-31 16:53:52 +0300

    Replace MdOutlinePerson icon with IoPerson icon in AboutMe component

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,9 +1,9 @@
-import { MdOutlinePerson } from "react-icons/md";
+import { IoPerson } from "react-icons/io5";
 
 const AboutMe = () => {
   return (
     <div className="flex items-center text-center mb-2 gap-1 text-gray-600">
-      <MdOutlinePerson className="text-xl" />
+      <IoPerson className="text-xl" />
       <p className=" font-bold text-sm">About Me</p>
     </div>
   );
