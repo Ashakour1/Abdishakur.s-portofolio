778b92e19d99e73508e518fa581f80db26f7461d
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-31 16:54:16 +0300

    Replace MdBusinessCenter icon with FaGraduationCap in Educations component

--- src/components/educations/educations.tsx
+++ src/components/educations/educations.tsx
@@ -1,4 +1,4 @@
-import { MdBusinessCenter } from "react-icons/md";
+import { FaGraduationCap } from "react-icons/fa";
 import { EducationsData } from "../../data/educations-data";
 import Education from "./education";
 
@@ -6,7 +6,7 @@ const Educations = () => {
   return (
     <section className="w-full  h-full">
       <div className="flex items-center text-center py-5 gap-1 text-gray-600 ">
-        <MdBusinessCenter className="text-xl" />
+        <FaGraduationCap className="text-xl" />
         <h1 className="font-bold text-sm">Education</h1>
       </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4">
