c950177d2cf1d426acf4ee2e3410c782301c288a
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-31 16:54:43 +0300

    Refactor ContactScreen layout to enhance user experience with improved styling and added ContactForm component

--- src/pages/ContactScreen.tsx
+++ src/pages/ContactScreen.tsx
@@ -1,7 +1,21 @@
+import { HiOutlineMail } from "react-icons/hi";
+
+import ContactForm from "../components/ContactForm";
+
 const ContactScreen = () => {
   return (
-    <div>
-      <h1>Contact Screen</h1>
+    <div className="max-w-[1020px] mx-auto px-5 py-10">
+      <div className="flex flex-col space-y-2 pb-5 ">
+        <div className="flex items-center text-center gap-1 text-gray-600">
+          <HiOutlineMail className="text-xl" />
+          <h1 className="font-bold text-sm">Contact Me</h1>
+        </div>
+        <p>
+          If you have any questions or want to work with me, feel free to send
+          me a message
+        </p>
+      </div>
+      <ContactForm />
     </div>
   );
 };
