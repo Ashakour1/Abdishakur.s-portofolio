a8cbc589fea31f48799ef92c66d5aa2a14e9982b
Ashakuur1 <engshakrayare114@gmail.com>
2024-12-31 22:38:04 +0300

    Replace sleep icon with GitHub link in Header component for improved navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { BiEnvelope, BiSearch } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
+// import { GiNightSleep } from "react-icons/gi";
 import { GoPerson } from "react-icons/go";
 import { Link } from "react-router-dom";
-import { GiNightSleep } from "react-icons/gi";
 
 const Header = () => {
+  // const { setTheme } = useTheme();
   return (
     <header>
       <div className="rounded-md flex  justify-between items-center px-5 max-w-[1020px] mx-auto h-16 mt-2">
@@ -43,7 +45,9 @@ const Header = () => {
         </nav>
         <div className="flex items-center space-x-4">
           <button className="border text-black px-3 py-3 rounded-full">
-            <GiNightSleep className="text-xl" />
+            <Link to="https://github.com/Ashakour1">
+              <FaGithub className="text-xl" />
+            </Link>
           </button>
         </div>
       </div>
