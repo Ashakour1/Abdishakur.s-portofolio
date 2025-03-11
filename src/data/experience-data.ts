c6bca05c506764d737866bb7f0de5200b3f8249a
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-11 22:18:46 +0300

    Update experience data with new roles and technologies

--- src/data/experience-data.ts
+++ src/data/experience-data.ts
@@ -2,31 +2,24 @@ import { type Experience } from "../types/experiences-type";
 
 export const ExperienceData: Experience[] = [
   {
-    position: "Fullstack Developer",
-    name: "Matso Organization",
-    logo: "/logo-matso.jpg",
-    time: "Fep 2022 - July 2024",
+    position: "Software Developer",
+    name: "Heegan Technology",
+    logo: "/logo-heegan.jpg",
+    time: "Jan 2025 - present",
     description:
-      "As a full-stack developer at Matso Organization, you'll design and develop high-quality software applications, collaborate with a skilled team, and stay updated with the latest industry trends. Your role involves writing efficient code, debugging issues, and delivering innovative solutions to maintain Matso Organization's leadership in the tech industry.",
-    technologies: ["Mysql", "Php", "Javascript", "React", "Prisma", "More"],
-    images: [],
-  },
-  {
-    position: "Founder & Developer",
-    name: "SomAtlasAPI",
-    logo: "/logo-somatlas.jpg",
-    time: "Dec 2023 - Present",
-    description:
-      "As the founder and developer of SomAtlasAPI, I developed a RESTful API to provide data on Somalia's regions, districts, and sub-districts. I designed the API to be user-friendly, secure, and scalable, ensuring that users can access accurate information with ease.",
+      "As a software developer at Heegan Technology, you'll design and develop high-quality software applications, collaborate with a skilled team, and stay updated with the latest industry trends. Your role involves writing efficient code, debugging issues, and delivering innovative solutions to maintain Heegan Technology's leadership in the tech industry.",
     technologies: [
-      "Svelte",
-      "Node.js",
+      "Mysql",
+      "React",
       "Prisma",
-      "supabase",
-      "Postgresql",
+      "Next.js",
+      "Python",
+      "Django",
+      "Flutter",
+      "Dart",
       "More",
     ],
-    images: ["/landing3.png", "/docs.png"],
+    images: [],
   },
   {
     position: "Software Developer",
@@ -47,5 +40,33 @@ export const ExperienceData: Experience[] = [
       "As the CDO of the Jamhuriya University Technology Students Association, I spearheaded digital advancements by developing a system to manage student data and events, creating the itDay event website to enhance participation, and implementing a secure voting system for efficient elections.",
     technologies: ["React", "Node.js", "Next.js", "MongoDb", "Prisma", "More"],
     images: ["/jutsa.png", "/jutsa-mn.png"],
+  },
+  
+  {
+    position: "Founder & Developer",
+    name: "SomAtlasAPI",
+    logo: "/logo-somatlas.jpg",
+    time: "Dec 2023 - Present",
+    description:
+      "As the founder and developer of SomAtlasAPI, I developed a RESTful API to provide data on Somalia's regions, districts, and sub-districts. I designed the API to be user-friendly, secure, and scalable, ensuring that users can access accurate information with ease.",
+    technologies: [
+      "Svelte",
+      "Node.js",
+      "Prisma",
+      "supabase",
+      "Postgresql",
+      "More",
+    ],
+    images: ["/landing3.png", "/docs.png"],
+  },
+  {
+    position: "Fullstack Developer",
+    name: "Matso Organization",
+    logo: "/logo-matso.jpg",
+    time: "Fep 2022 - July 2024",
+    description:
+      "As a full-stack developer at Matso Organization, you'll design and develop high-quality software applications, collaborate with a skilled team, and stay updated with the latest industry trends. Your role involves writing efficient code, debugging issues, and delivering innovative solutions to maintain Matso Organization's leadership in the tech industry.",
+    technologies: ["Mysql", "Php", "Javascript", "React", "Prisma", "More"],
+    images: [],
   },
 ];
